2a784d564a6509e86483e332e01f0c5aeff8c935
fullstackdassi1995 <112422387+fullstackdassi1995@users.noreply.github.com>
2023-03-14 15:39:08 +0200

    1

--- src/test/java/JUnit.java
+++ src/test/java/JUnit.java
@@ -10,11 +10,17 @@ public class JUnit {
      public void tryAdd(){
          MyLimitedList <Integer>  lis1 = new MyLimitedList<>(3);
          lis1.addItem(3);
-//       ArrayList<Integer> expected = new ArrayList<>();
-//       expected.add(3);
         int expected = 3;
         int actual = lis1.getItem(0);
         Assertions.assertEquals(expected, actual);
-
+        }
+        @Test
+    public void tryRemove(){
+        MyLimitedList <Integer>  lis1 = new MyLimitedList<>(3);
+        lis1.addItem(56);lis1.addItem(18);lis1.addItem(05);
+        lis1.removeItem();
+        int expected = 2;
+        int actual = lis1.size();
+        Assertions.assertEquals(expected, actual);
     }
 }
